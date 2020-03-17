244624925aacb7aafd52be4c9bc18bfd8fa6a310
Jason Peters <JasonPeters@Lindsays-MacBook-Pro.local>
2020-03-16 18:35:22 -0700

    finsihed part 1

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,27 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+var tbody = d3.select("tbody");
+
+// Select the button
+var button = d3.select("#filter-btn");
+
+button.on("click", function() {
+
+  // Select the input element and get the raw HTML node
+  var inputElement = d3.select("#datetime");
+
+  // Get the value property of the input element
+  var inputValue = inputElement.property("value");
+
+  // Filter the results to match the input
+  var filteredData = tableData.filter(sighting => sighting.datetime === inputValue);
+
+  filteredData.forEach((record) => {
+    var row = tbody.append("tr");
+    Object.entries(record).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+    });
+});
